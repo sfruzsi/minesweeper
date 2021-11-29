658fa5cf1f874c148f78d021cf1e188580f784dd
Fruzsina Strbik <fruzsina.strbik1@ibm.com>
2021-11-30 00:17:53 +0100

    feat: red

--- src/minesweeper.test.js
+++ src/minesweeper.test.js
@@ -55,5 +55,9 @@ describe('Testing Mine Sweeper', () => {
         '+-+-+-+\n| | | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| | | |\n+-+-+-+'
       );
     });
+
+    it('Given board with mine on [1,1] When I click on [0,0] Then cell cleared returns', () => {
+      expect(game.clickCell(1, 1)).toBe('Cleared cell');
+    });
   });
 });
